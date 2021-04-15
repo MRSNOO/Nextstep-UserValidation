4f4e95969499daf010724dc631dd4113ce7106a8
Quang Nguyen <quangnguyen@Quangs-MacBook-Pro.local>
2021-04-15 22:26:56 +0700

    .

--- demo/public/app.js
+++ demo/public/app.js
@@ -45,40 +45,6 @@ function getUiConfig() {
         // Required to enable ID token credentials for this provider.
         clientId: CLIENT_ID
       },
-      {
-        provider: firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-        scopes :[
-          'public_profile',
-          'email',
-          'user_likes',
-          'user_friends'
-        ]
-      },
-      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-      firebase.auth.GithubAuthProvider.PROVIDER_ID,
-      {
-        provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-        // Whether the display name should be displayed in Sign Up page.
-        requireDisplayName: true,
-        signInMethod: getEmailSignInMethod(),
-        disableSignUp: {
-          status: getDisableSignUpStatus()
-        }
-      },
-      {
-        provider: firebase.auth.PhoneAuthProvider.PROVIDER_ID,
-        recaptchaParameters: {
-          size: getRecaptchaMode()
-        },
-      },
-      {
-        provider: 'microsoft.com',
-        loginHintKey: 'login_hint'
-      },
-      {
-        provider: 'apple.com',
-      },
-      firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
     ],
     // Terms of service url.
     'tosUrl': 'https://www.google.com',
@@ -89,6 +55,8 @@ function getUiConfig() {
         firebaseui.auth.CredentialHelper.NONE
   };
 }
+
+const LIST_VALID_EMAIL = ["qnn1@rice.edu"]
 
 // Initialize the FirebaseUI Widget using Firebase.
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
@@ -161,9 +129,14 @@ var handleSignedOutUser = function() {
 // Listen to change in auth state so it displays the correct UI for when
 // the user is signed in or not.
 firebase.auth().onAuthStateChanged(function(user) {
+  var checkAccount = true
   document.getElementById('loading').style.display = 'none';
   document.getElementById('loaded').style.display = 'block';
-  user ? handleSignedInUser(user) : handleSignedOutUser();
+  if (user && !LIST_VALID_EMAIL.includes(user.email)){
+    alert("Your email is not authorized to enter the page")
+    checkAccount = false
+  }
+  user && checkAccount ? handleSignedInUser(user) : handleSignedOutUser();
 });
 
 /**
